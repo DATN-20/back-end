468e02c44b035feed9fa610f5c80529a8d6bd57c
hao-truong <hao-truong@cybozu.vn>
2024-02-21 18:07:31 +0700

    SPR-2.6: ComfyUIService generateTextToImage

--- src/core/module/ai-generate-image/socket/ComfyUISocket.ts
+++ src/core/module/ai-generate-image/socket/ComfyUISocket.ts
@@ -2,6 +2,12 @@ import { ComfyUIConfig } from '@infrastructure/config/ComfyUIConfig';
 import { BaseSocketClient } from '@infrastructure/socket/BaseSocketClient';
 import { v4 as uuidv4 } from 'uuid';
 
+export enum ComfyUITypeMessageSocket {
+  EXECUTING = 'executing',
+  PROGRESS = 'progress',
+  EXECUTED = 'executed',
+}
+
 export class ComfyUISokcet extends BaseSocketClient {
   protected clientId: string;
 
@@ -12,5 +18,19 @@ export class ComfyUISokcet extends BaseSocketClient {
       `wss://${ComfyUIConfig.COMFYUI_URL}/ws?clientId=${client_id}`,
     );
     this.clientId = client_id;
+  }
+
+  public getExecutedResultFromMessage(
+    prompt_id: string,
+    callback: (output_images: any) => void,
+  ): void {
+    this.webSocket.on('message', (message) => {
+      const { type, data } = JSON.parse(message.toString('utf-8'));
+
+      if (type === ComfyUITypeMessageSocket.EXECUTED && data.prompt_id === prompt_id) {
+        const output_images = data.output.images;
+        callback(output_images);
+      }
+    });
   }
 }
