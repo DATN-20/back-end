2a1e5cec6aa512e1078bdcc606e6774cfb4679b4
setqt <tqthai1852@gmail.com>
2024-02-25 15:07:02 +0700

    fix: change email

--- src/infrastructure/config/MailConfig.ts
+++ src/infrastructure/config/MailConfig.ts
@@ -4,10 +4,10 @@ export class MailConfig {
   public static readonly MAIL_HOST: string = get('MAIL_HOST').default('smtp.gmail.com').asString();
   public static readonly MAIL_PORT: string = get('MAIL_PORT').default('587').asString();
   public static readonly MAIL_USER: string = get('MAIL_USER')
-    .default('artisticaihubs@gmail.com')
+    .default('buitiendat141202@gmail.com')
     .asString();
   public static readonly MAIL_PASSWORD: string = get('MAIL_PASSWORD')
-    .default('ijfv ngjo dkre axdb')
+    .default('qjky himg ftzg yecj')
     .asString();
   public static readonly MAIL_FROM: string = '"No Reply" <noreply@example.com>';
 }
