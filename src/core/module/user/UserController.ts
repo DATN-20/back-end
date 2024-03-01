3cd449bfe62e226d29a79fe1c68367a57bc65b49
hao-truong <hao-truong@cybozu.vn>
2024-03-01 15:47:05 +0700

    SPR-2.15: Get UserProfile

--- src/core/module/user/UserController.ts
+++ src/core/module/user/UserController.ts
@@ -1,7 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { UserService } from './UserService';
+import { AuthGuard } from '@core/common/guard/AuthGuard';
+import { User } from '@core/common/decorator/UserDecorator';
 
+@UseGuards(AuthGuard)
 @Controller('users')
 export class UserController {
   public constructor(private readonly userService: UserService) {}
+
+  @Get('me')
+  async getLoggedInUserProfile(@User() user: UserFromAuthGuard) {
+    return await this.userService.handleGetLoggedInUserProfile(user.id);
+  }
 }
---
+++ src/core/module/user/entity/response/UserProfileResponse.ts
@@ -0,0 +1,30 @@
+import { DateUtil } from '@core/common/util/DateUtil';
+import { User } from '../User';
+
+export class UserProfileResponse {
+  public firstName: string;
+  public lastName: string;
+  public aliasName: string;
+  public phone: string;
+  public address: string;
+  public description: string;
+  public socials: Social[];
+  public role: string;
+  public createdAt: string;
+  public updatedAt: string;
+
+  public static convertToResponseFromUserEntity(user: User): UserProfileResponse {
+    const result = new UserProfileResponse();
+    result.firstName = user.firstName;
+    result.lastName = user.lastName;
+    result.phone = user.phone;
+    result.address = user.address;
+    result.description = user.description;
+    result.socials = user.socials;
+    result.role = user.role;
+    result.createdAt = DateUtil.formatDate(user.createdAt);
+    result.updatedAt = DateUtil.formatDate(user.updatedAt);
+
+    return result;
+  }
+}
