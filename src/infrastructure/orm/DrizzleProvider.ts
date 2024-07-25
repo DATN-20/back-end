90b3da22b23cf83bd4e8a8b05d299e8874b1f60d
hao-truong <hao-truong@cybozu.vn>
2024-07-25 13:57:31 +0700

    SPR-bug: The client was disconnected by the server because of inactivity. See wait_timeout and interactive_timeout for configuring this behavior.

--- src/infrastructure/orm/DrizzleProvider.ts
+++ src/infrastructure/orm/DrizzleProvider.ts
@@ -1,10 +1,7 @@
-import { createConnection } from 'mysql2/promise';
+import mysql from 'mysql2/promise';
 import { drizzle, MySql2Database } from 'drizzle-orm/mysql2';
 import * as schema from './schema';
 import { DatabaseConfig } from '@infrastructure/config/DatabaseConfig';
-import SystemLogger from '@core/common/logger/SystemLoggerService';
-import { DatabaseError } from '@core/common/resource/error/DatabaseError';
-import { LogType } from '@core/common/enum/LogType';
 
 export const DrizzleAsyncProvider = 'drizzleProvider';
 
@@ -12,38 +9,20 @@ export const drizzleProvider = [
   {
     provide: DrizzleAsyncProvider,
     useFactory: async (): Promise<MySql2Database<typeof schema>> => {
-      const createDbConnection = async () => {
-        const connection = await createConnection({
-          host: DatabaseConfig.DATABASE_HOST,
-          user: DatabaseConfig.DATABASE_USER,
-          database: DatabaseConfig.DATABASE_NAME,
-          password: DatabaseConfig.DATABASE_PASSWORD,
-          port: DatabaseConfig.DATABASE_PORT,
-          charset: 'utf8mb4',
-          keepAliveInitialDelay: 3 * 1000,
-          enableKeepAlive: true,
-          idleTimeout: 5 * 60 * 1000,
-          maxIdle: 0,
-        });
+      const poolConnection = mysql.createPool({
+        host: DatabaseConfig.DATABASE_HOST,
+        user: DatabaseConfig.DATABASE_USER,
+        database: DatabaseConfig.DATABASE_NAME,
+        password: DatabaseConfig.DATABASE_PASSWORD,
+        port: DatabaseConfig.DATABASE_PORT,
+        charset: 'utf8mb4',
+        keepAliveInitialDelay: 3 * 1000,
+        enableKeepAlive: true,
+        maxIdle: 10,
+        connectionLimit: 10,
+      });
 
-        connection.on('error', async err => {
-          if (err.code === 'PROTOCOL_CONNECTION_LOST' || err.code === 'ECONNRESET') {
-            SystemLogger.error(err.message, {
-              error_code: DatabaseError.DATABASE_CONNECTION_LOST.error_code,
-              back_trace: 'src/infrastructure/orm/DrizzleProvider.ts',
-              log_type: LogType.SYSTEM,
-            });
-            await createDbConnection();
-          } else {
-            throw err;
-          }
-        });
-
-        return connection;
-      };
-
-      const connection = await createDbConnection();
-      const db = drizzle(connection, { schema, mode: 'default' });
+      const db = drizzle(poolConnection, { schema, mode: 'default' });
 
       return db;
     },
